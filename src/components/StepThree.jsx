4b71745b249572c4983a66ab048686bff4bb6809
MikkelRuby <mikkelruby1@hotmail.com>
2024-10-17 11:21:38 +0200

    Update StepThree.jsx

--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -2,44 +2,48 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function StepThree() {
-
   const [activeIcon, setActiveIcon] = useState(null);
 
   return (
     <section>
       <div className="btn-boks">
-      <button className="image-button">
-        <img src="/wire.webp" class="button-image" alt="Button Image" />
-      </button>
+        <button className="image-button">
+          <img src="/wire.webp" className="button-image" alt="Button Image" />
+        </button>
       </div>
       <div className="stepthree-container">
-      <div className="stepbystep-boks">
-        <div className="filled3"></div>
+        <div className="stepbystep-boks">
+          <div className="filled3"></div>
+        </div>
+        <h1 className="stepthreeh1">Dato & Sted</h1>
       </div>
-      <h1 className="stepthreeh1">Dato & Sted</h1>
-      </div>
-
 
       <div className="boks-container-stepthree">
         <div>
-        <div className={`boks-stepthree ${activeIcon === 0 ? "active" : ""}`}
-        onClick={()=> setActiveIcon(0)}>
+          <div
+            className={`boks-stepthree ${activeIcon === 0 ? "active" : ""}`}
+            onClick={() => setActiveIcon(0)}
+          >
             <img src="/hammer.webp" alt="Hammer" />
           </div>
           <p className="undertekst-stepthree">Haster</p>
         </div>
 
         <div>
-        <div className={`boks-stepthree ${activeIcon === 1 ? "active" : ""}`}
-        onClick={()=> setActiveIcon(1)}>
+          <div
+            className={`boks-stepthree ${activeIcon === 1 ? "active" : ""}`}
+            onClick={() => setActiveIcon(1)}
+          >
             <img src="/pruning-shears.webp" alt="Havesaks" />
           </div>
           <p className="undertekst-stepthree">Fleksibel</p>
         </div>
 
         <div>
-        <div className={`boks-stepthree ${activeIcon === 2 ? "active" : ""}`}
-        onClick={()=> setActiveIcon(2)}>
+          <div
+            className={`boks-stepthree ${activeIcon === 2 ? "active" : ""}`}
+            onClick={() => setActiveIcon(2)}
+          >
             <img src="/spanner.webp" alt="Spanner" />
           </div>
           <p className="undertekst-stepthree">Vælg dato</p>
