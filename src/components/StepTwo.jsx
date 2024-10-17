93082c8b7919a699af14071c19e3c1e7beffb40f
AndersFlaeng <eaa23aaf@students.eaaa.dk>
2024-10-17 09:30:49 +0200

    Tis

--- src/components/StepTwo.jsx
+++ src/components/StepTwo.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function StepTwo() {
   return (
-    <section>
-      <h1>Vælg Kategori</h1>
-    </section>
+<section className="step1section">
+  <button class="image-button">
+    <img src="/wire.webp" class="button-image" alt="Button Image"/>
+  </button>
+  <div className="stepbystep-boks">
+    <div className="filled2"></div>
+  </div>
+
+  <h1 className="step1h1">Beskriv din opgave</h1>
+
+  <div class="beskriv-boks">
+  <textarea class="tekst" placeholder="Beskriv her..."></textarea>
+</div>
+</section>
   );
 }
