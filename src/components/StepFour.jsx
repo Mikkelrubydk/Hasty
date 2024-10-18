73e9aa85aac714fce01cd93c79cdbe7c28de6f46
AndersFlaeng <eaa23aaf@students.eaaa.dk>
2024-10-18 11:41:14 +0200

    .

--- src/components/StepFour.jsx
+++ src/components/StepFour.jsx
@@ -3,10 +3,6 @@ import { Link } from "react-router-dom";
 export default function StepFour() {
   return (
     <section className="step2section">
-      <div className="stepbystep-boks">
-        <div className="filled4"></div>
-      </div>
-
       <h1 className="step1h1">Uddyb beskrivelse</h1>
       <h3>Uddybende beskrivelse af opgaven, så opgaven kan blive løst lige som du ønsker det</h3>
       <div className="beskriv-boks">
