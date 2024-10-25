b1985bc6e582a1f4161752bc3cc26ce8334e699d
MikkelRuby <mikkelruby1@hotmail.com>
2024-10-25 10:56:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Hasty/", // Set the base path for GitHub Pages
+  base: "/", // Set the base path for GitHub Pages
 });
